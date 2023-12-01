5a249cf17dc2388afac7255cc2b84994190c595c
Abubakarl <Qsssoftnic@gmail.com>
2023-12-01 21:41:54 +0500

    loading bug fixed

--- lib/functions/get_video_data.dart
+++ lib/functions/get_video_data.dart
@@ -20,7 +20,7 @@ getData(String userUrl, BuildContext context) async {
       context,
       errorMessage: "Invalid URL Please try again with a valid URL.",
     );
-    appStates.startLoading();
+    appStates.stopLoading();
     return;
   }
   debugPrint(uriHost);
@@ -28,7 +28,7 @@ getData(String userUrl, BuildContext context) async {
     errorSheet(context,
         errorMessage:
             "Facebook videos downloading is currently not supported. support coming soon.");
-    appStates.startLoading();
+    appStates.stopLoading();
   } else if (uriHost == "m.youtube.com" ||
       uriHost == "youtu.be" ||
       uriHost == "youtube.com") {
@@ -37,7 +37,7 @@ getData(String userUrl, BuildContext context) async {
       errorMessage:
           "Due to legal restrictions, we can't download videos from YouTube. Please use a different link.",
     );
-    appStates.startLoading();
+    appStates.stopLoading();
   } else {
     getVideoData(extractedUrl: userUrl, context: context);
   }
@@ -97,13 +97,13 @@ void getVideoData(
           kpage: "Instagram",
           context: context,
         );
-        appStates.startLoading();
+        appStates.stopLoading();
       } else {
         errorSheet(
           context,
           errorMessage: "Invalid URL Please try again with a valid URL.",
         );
-        appStates.startLoading();
+        appStates.stopLoading();
         return;
       }
     }
@@ -115,5 +115,5 @@ void getVideoData(
           "There is some issue while fetching data. Please try again later.",
     );
   }
-  appStates.startLoading();
+  appStates.stopLoading();
 }
